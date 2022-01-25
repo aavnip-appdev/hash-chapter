4d4db150b37f85431185f7f961ac1ced2efa3e63
aavnip <aavnip@chicagobooth.edu>
2022-01-25 04:17:08 +0000

    Hash WIP Person Dig

--- hash_dig.rb
+++ hash_dig.rb
@@ -1,13 +1,25 @@
 # Write a program that uses the sample_hash variable and prints the value of key "history"
 
 sample_hash = {
-   :class => { 
-      :student => { 
-         :name => "Mike",
-         "marks" => { 
-            "physics" => 70,
-            "history" => 80
-         }
-      }
-   }
+  :class => {
+    :student => {
+      :name => "Mike",
+      "marks" => {
+        "physics" => 70,
+        "history" => 80,
+      },
+    },
+  },
 }
+
+# Method 1
+p sample_hash.keys
+p class_hash = sample_hash.fetch(:class)
+p class_hash.keys
+p student_hash = class_hash.fetch(:student)
+p student_hash.keys
+p marks_hash = student_hash.fetch("marks")
+p history_hash = marks_hash.fetch("history")
+
+# Method 2
+p sample_hash.fetch(:class).fetch(:student).fetch("marks").fetch("history")
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,6 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+person1_input = gets.chomp.split 
+p person1_input
+person1 = { :name => person1_input[0], :age => person1_input[1].to_i, :occupation => person1_input[2] }
+p person1
